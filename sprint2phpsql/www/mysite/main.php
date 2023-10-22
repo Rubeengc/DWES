f4672bf09f457ac1de81c99b56a843033497c14d
RubénGarcia <rgarciac@fpcoruna.afundacion.org>
2023-10-22 15:48:29 +0200

    Ejercicio 2

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,8 +1,23 @@
-<?php
+  <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
 <html>
 	<body>
 		<h1>Conexion establecida</h1>
+		<?php
+		$query = 'SELECT * FROM tPeliculas';
+		$result=mysqli_query($db,$query) or die ('Query error');
+		while ($row = mysqli_fetch_array ($result)){
+		echo $row['nombre'];
+		echo '<br>';
+		echo '<img src ='. $row[2].'></img>';
+		echo '<br>';
+		echo $row[3];
+		echo '<br>';
+		echo $row[4];
+ 		echo '<hr>';
+		}
+		?>
+		mysqli_close($db);
 	</body>
 </html>
