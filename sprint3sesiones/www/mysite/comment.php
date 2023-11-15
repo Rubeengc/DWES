0080f63bf9f0aec6d6acd2e1cf995e4a74a643b7
ruben <ruben@entonografico.myguest.virtualbox.org>
2023-11-15 17:29:44 +0100

    Ejercicio#4:Creo la funcionalidad en comment.php

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -1,25 +1,26 @@
 <?php
- $db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+session_start(); 
+// Verificar si el usuario está logueado
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+} else {
+    // Si no está logueado, redirigir a la página de inicio de sesión
+    header("Location: login.html");
+    exit();
+}
+$peliculas_id = $_POST['peliculas_id'];
+$comentario = $_POST['new_comment'];
+$now = new DateTime("now");
+$now = $now->format('ymd');
+$query = "INSERT INTO tComentarios (comentario, usuario_id, peliculas_id, fecha) VALUES (".$comentario.", ".$user_id_a_insertar.", ".$peliculas_id.", ".$now.")";
+$result = mysqli_query($db, $query);
+if ($result) {
+    echo "<p>Nuevo comentario añadido</p>";
+} else {
+    echo "<p>Error al añadir el comentario</p>";
+}
+echo "<a href='/detail.php?peliculas_id=".$peliculas_id."'>Volver</a>";
+mysqli_close($db);
 ?>
-<html>
-	<body>
-		<?php
-		  $peliculas_id = $_POST['peliculas_id'];
-		  $comentario = $_POST['new_comment'];
-		  $now = new DateTime("now");
-		  $now = $now -> format('ymd');
-	 	  $query = "INSERT INTO tComentarios (comentario,usuario_id,peliculas_id,fecha) VALUES ('".$comentario."',NULL,".$peliculas_id.",'".$now."')";
-		  mysqli_query($db,$query) or die('Error');
-		  echo "<p>Nuevo comentario ";
-		  echo mysqli_insert_id($db);
-		  echo " añadido</p>";
-		  echo "<a href='/detail.php?peliculas_id=".$peliculas_id."'>Volver</a>";
-		  mysqli_close($db);
-		?>
-
-</body>
-
-
-
-
-</html>
